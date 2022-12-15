d82a10f6b636c767ae0bc4a28dcb0c8f5133190a
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-15 17:23:12 +0500

    Account details completed

--- src/Components/Account Details/AccountDetails.js
+++ src/Components/Account Details/AccountDetails.js
@@ -9,6 +9,17 @@ const AccountDetails = () => {
         <h3>Your Balance</h3>
         <h4>0$</h4>
       </div>
+
+      <div className="account-info">
+        <div className="income">
+          <p>Income</p>
+          <p className="total-income">500$</p>
+        </div>
+        <div className="expense">
+          <p>Expense</p>
+          <p className="total-expense">200$</p>
+        </div>
+      </div>
     </div>
   );
 };
