66eda1bddef6ea8471749f6e2b8b0d0abd55b907
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-15 16:12:18 +0500

    Logo added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="logo">
+        <h1>Expense Tracker</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
