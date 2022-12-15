ebd8ac48216d243b97d84f3015a246cdd126dac2
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-15 17:33:18 +0500

    added history component

---
+++ src/Components/History/History.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const History = () => {
+  return (
+    <div className="history-container">
+      <h3 className="history-heading">History</h3>
+    </div>
+  );
+};
+
+export default History;
