90999dedb06504fffbdda421f0d694f23bbafd27
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-16 11:57:53 +0500

    Getting data from useSelector State to AccountDetails

--- src/Components/Account Details/AccountDetails.js
+++ src/Components/Account Details/AccountDetails.js
@@ -1,23 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import "./account.css";
 
 const AccountDetails = () => {
+  const account = useSelector((state) => state.reducer);
   return (
     <div className="account-container">
       <div>
         <h3>Your Balance</h3>
-        <h4>0$</h4>
+        <h4>{account.balance}$</h4>
       </div>
 
       <div className="account-info">
         <div className="income">
           <p>Income</p>
-          <p className="total-income">500$</p>
+          <p className="total-income">{account.income}$</p>
         </div>
         <div className="expense">
           <p>Expense</p>
-          <p className="total-expense">200$</p>
+          <p className="total-expense">{account.expense}$</p>
         </div>
       </div>
     </div>
--- src/state/reducers/transactionReducer.js
+++ src/state/reducers/transactionReducer.js
@@ -1,4 +1,7 @@
-export const transactionReducer = (state = {}, action) => {
+export const transactionReducer = (
+  state = { balance: 0, income: 0, expense: 0 },
+  action
+) => {
   switch (action.type) {
     case "MAKE_TRANSACTION":
       console.log([action.payload, ...state]);
